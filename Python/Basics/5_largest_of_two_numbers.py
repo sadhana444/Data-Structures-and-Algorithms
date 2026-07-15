4bb6caf6e3ad8f5415bd44d19a306495ae952f56
Sadhana <sadhanaraja2005@gmail.com>
2026-07-15 18:07:25 +0530

    Add Largest of Two Numbers

--- Python/Basics/5_largest_of_two_numbers.py
+++ Python/Basics/5_largest_of_two_numbers.py
@@ -1,1 +1,8 @@
-
+num1=int(input("enter a number 1:"))
+num2=int(input("enter a number 2:"))
+if num1>num2:
+    print("Num1 is greater than Num2")
+elif num2>num1:
+     print("Num2 is greater than Num1")
+else:
+    print("Num1 and Num2 are equal")
