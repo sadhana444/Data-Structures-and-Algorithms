c1e989dd6e11ea913da5f8b91bf501ae0a1a8874
Sadhana <sadhanaraja2005@gmail.com>
2026-07-16 11:30:46 +0530

    Add functionality to find the largest of three numbers

--- Python/Basics/6_largest_of_three_numbers.py
+++ Python/Basics/6_largest_of_three_numbers.py
@@ -1,1 +1,11 @@
-
+num1=int(input("Enter a 1st number:"))
+num2=int(input("Enter a 2nd number:"))
+num3=int(input("Enter a 3rd number:"))
+if num1>num2 and num1>num3:
+    print(num1,"is greatest number")
+elif num2>num1 and num2>num3:
+    print(num2, "is greatest number")
+elif num3>num2 and num3>num1:
+    print(num3, "is greatest number")    
+else:
+    print("All numbers are equal")
