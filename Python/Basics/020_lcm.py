02d9b2d5115bc1731db34f7640edd0bea96f07de
Sadhana <sadhanaraja2005@gmail.com>
2026-07-20 22:10:03 +0530

    LCM

--- Python/Basics/020_lcm.py
+++ Python/Basics/020_lcm.py
@@ -1,1 +1,8 @@
-
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+lcm=max(num1, num2)
+while True:
+    if lcm%num1==0 and lcm%num2==0:
+        break
+    lcm=lcm+1
+print("LCM is:",lcm)
