6ff0048ad8bfbcf2bff37c4f55385b8c5fe126c7
Sadhana <sadhanaraja2005@gmail.com>
2026-07-20 22:03:14 +0530

    GCD

--- Python/Basics/019_gcd.py
+++ Python/Basics/019_gcd.py
@@ -1,1 +1,8 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+gcd=0
+for i in range(1,min(num1,num2)+1):
+  if num1%i==0 and num2%i==0:
+    gcd=i
+print("GCD is:",gcd)
 
